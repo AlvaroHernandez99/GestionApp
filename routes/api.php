83b722d7c2a1647e5b4863a8227743f4ad5e02a8
alvaro hernandez <alvarohernandezmurcia@gmail.com>
2022-12-18 15:09:37 +0100

    arreglar modify y readme

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/customers', [CustomerController::class, 'getAll']);
+Route::get('/customers/{id}', [CustomerController::class, 'getId']);
+Route::post('/customers', [CustomerController::class, 'create']);
+Route::delete('/customers/{id}', [CustomerController::class, 'delete']);
+Route::patch('/customers/{id}', [CustomerController::class, 'modify']);
+
+//Route::middleware('getId')->get('/customers/{id}', [CustomerController::class, 'getId']);
+
+
